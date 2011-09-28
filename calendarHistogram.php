a909d8330d03b73ce5214ff40fcba5ca34fbe63a
Scott Brenner <scott@scottbrenner.com>
2011-09-27 20:22:48 -0600

    added a new library

--- calendarHistogram.php
+++ calendarHistogram.php
@@ -20,7 +20,7 @@ class PDF extends FPDF
 	    // Move to the right
 	    $this->Cell(80);
 	    // Title  Prepared on: " . date('l F j') . ". 
-	    $title = "Judge " . ucfirst(htmlspecialchars($_GET["judge"])) . " - Criminal Trial Settings Histogram";
+	    $title = "Judge " . ucfirst($this->judge) . " - Criminal Trial Settings Histogram";
 	    $memo = "No criminal jury trials on Mondays.  No more that 15 settings per day.";
 	    $this->Cell(30,5,$title,0,1,'C');
 	    $this->Cell(80);
@@ -28,11 +28,6 @@ class PDF extends FPDF
 	    // Line break
 	    $this->Ln(5);
 	}
-
-
-    function setTotal($number){
-        $this->totalSettings = $number;
-    }
 
 	function Footer()
 	{
@@ -47,7 +42,14 @@ class PDF extends FPDF
 	    // Line break
 	    $this->Ln(10);
 	}
-		
+
+    function setJudge($judge){
+        $this->judge = $judge;
+    }
+
+    function setTotal($number){
+        $this->totalSettings = $number;
+    }
 	
 	// Colored table
 	function FancyTable($header, $data)
@@ -100,14 +102,39 @@ class PDF extends FPDF
 
 class activityHistogram
 {
-	var $NACData;
-	
 	// Contruct object
 	function __construct() {
-		// Returns the most recent monday, or today if today is Monday
+	    if (isset($_GET["judge"])){
+	        if ($_GET["judge"] == "all"){
+	            $judgesArray = self::getJudgesNames();
+	        }
+	        else{
+	        $judgesArray[] = htmlspecialchars($_GET["judge"]);
+	        }
+	    }
+	    else{
+	        $judgesArray[] = "Allen";
+	    }
+	    
+	    $pdf = new PDF();
+        $header = array('Date', 'Criminal Settings','', 'Notes');
+	    foreach ($judgesArray as $key => $value) {
+	        $pdf->setJudge($value);
+	        $pdf->setTotal($NACandTotal[1]);
+            $pdf->AddPage();
+            $NACandTotal = self::createNACData($value);
+            $pdf->setTotal($NACandTotal[1]);
+            // print_r ($data);
+    		$pdf->FancyTable($header, $NACandTotal[0]);
+	    }
+		$pdf->Output();
+	}
+		
+	protected function createNACData($judgeName){
+	    // Returns the most recent monday, or today if today is Monday
 		$startDate = date("Y-m-d", strtotime('last monday', strtotime('tomorrow')));
 		
-		// Create an array with weekdays as keys for the next 60 weekdays
+        // Create an array with weekdays as keys for the next 60 weekdays
 		for ($i=0, $j=0; $i<50; $i++, $j++) {
            $nextDay = strtotime('+' . $j . ' days', strtotime('last monday', strtotime('tomorrow')));
            if (date('w', $nextDay) > 0 && date('w', $nextDay) < 6) {
@@ -117,28 +144,20 @@ class activityHistogram
                $i--;
            }
         }
-        
         end($NACData);          // move the internal pointer to the end of the array
         $endDate = key($NACData);
-        reset($NACData);        // move the pointer back to the start
-        // print_r($NACData);
-        $NACandTotal = self::createNACData( $startDate, $endDate, $NACData);        
-        self::createPDF($NACandTotal);
-	}
-	
-	protected function createNACData( $startDate, $endDate, $NACDataPassed ){
+        reset($NACData);
+        
 		// Build the query.
 		$query = "SELECT NAC_date, NAC 
 			FROM nextActions WHERE
 			NAC_date between '{$startDate}' 
 			and '{$endDate }' 
-			AND case_number like '%B %'";
-			
-		// If judge is given append and ... to the query
-        if(isset($_GET["judge"])){
-            $judgeReq = htmlspecialchars($_GET["judge"]);
-            $query = $query . " AND judge like '%{$judgeReq}%'";
-        }else{$query = $query . " AND judge like '%Allen%'";}
+			AND case_number like '%B %'
+			AND judge like '%{$judgeName}%'";
+        
+        //echo $query;
+		
         
 		/*** connect to MySql database ***/
 		try 
@@ -154,24 +173,20 @@ class activityHistogram
 		    die( "<br><br>Query Closed !!! $error");
 		}
 
-		// Query the database and loop through the results.
-		// $result = mysql_unbuffered_query($query) or die(mysql_error());
-		
-		//echo $query;
-		
+		// Query the database and loop through the results.		
 		if($result = mysql_unbuffered_query($query)){
-        // print_r ($NACDataPassed);
+        // print_r ($NACData);
         
         // Add histogram to NACData dictionary
 		   while($row = mysql_fetch_array( $result, MYSQL_ASSOC )){
-		   	$NACDataPassed[substr($row["NAC_date"], 0, 10)] .= 
-		   		self::getLetter($row["NAC"]);
+		   	$NACData[substr($row["NAC_date"], 0, 10)] .= 
+                self::getLetter($row["NAC"]);
 		   }
 		}
 		// Convert dictionary to array of arrays 
 		$notes = "";
 		$totalSettings = 0;
-		foreach ($NACDataPassed as $key => $value){
+		foreach ($NACData as $key => $value){
             $results[] = array($key, self::sortString($value),  strlen($value) ,$notes);
             $totalSettings += (int)strlen($value);
 		}
@@ -201,20 +216,27 @@ class activityHistogram
 		return "*";
 	}
 
-	function createPDF($dataArray){
-	    $data = $dataArray[0];
-        $totalSettings = $dataArray[1];
-		$pdf = new PDF();
-		// Column headings
-		$header = array('Date', 'Criminal Settings','', 'Notes');
-		$pdf->setTotal($totalSettings);
-		$pdf->AddPage();
-        // print_r ($data);
-		$pdf->FancyTable($header, $data);
-		$pdf->Output();
-	}
+    protected function getJudgesNames(){
+        return array("ALLEN/NADINE",
+        "BURKE/KIM/WILSON",
+        "COOPER/ETHNA/M",
+        "DEWINE/PAT",
+        "HELMICK/DENNIS/S",
+        "KUBICKI JR/CHARLES/J",
+        "LUEBBERS/JODY/M",
+        "MARSH/MELBA/D",
+        "MARTIN/STEVEN/E",
+        "METZ/JEROME/J",
+        "MYERS/BETH/A",
+        "NADEL/NORBERT/A",
+        "RUEHLMAN/ROBERT/P",
+        "WEST/JOHN/ANDREW",
+        "WINKLER/RALPH",
+        "WINKLER/RALPH/E",
+        "WINKLER/ROBERT/C");
+    }
 
-	function sortString($string){
+	protected function sortString($string){
 		for ($i = 0; $i <= strlen($string); $i++) {
 			$myArray[] = $string[$i];
 		}
